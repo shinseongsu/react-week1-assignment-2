a468562cf7e6b78952d6e2166a77cdbd64f13531
Shin seongsu <tlstjdtn321@naver.com>
2024-04-21 03:09:31 +0900

    feat: 사용될 객체

---
+++ src/domain/Calculation.js
@@ -0,0 +1,9 @@
+class Calculation {
+  constructor(accumulator, number, operator) {
+    this.accumulator = accumulator;
+    this.number = number;
+    this.operator = operator;
+  }
+}
+
+export default Calculation;
