5bd8c76f8159cb0bf433aae322b6ebff13be28cf
Shin seongsu <tlstjdtn321@naver.com>
2024-04-21 03:09:56 +0900

    feat: 필요한 함수 추가

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension, no-unused-vars */
 
 /* @jsx createElement */
+import calculator from './domain/Calculator';
+import Numbers from './domain/Numbers';
+import Operators from './domain/Operators';
+import Calculation from './domain/Calculation';
 
 function createElement(tagName, props, ...children) {
   const element = document.createElement(tagName);
@@ -20,10 +24,33 @@ function createElement(tagName, props, ...children) {
   return element;
 }
 
-function render() {
+function render({ accumulator, number, operator }) {
+  function clickNumberHandler({ value }) {
+    render(new Calculation(accumulator, number*10+value, operator));
+  }
+
+  function clickOperatorHandler({ value }) {
+    render(new Calculation(calculator(accumulator, number, operator), 0, value));
+  }
+
   const element = (
     <div>
       <p>간단 계산기</p>
+      <div>{number || accumulator}</div>
+      <div>
+        {Numbers.map((num) => (
+          <button onClick={() => clickNumberHandler({ value: num })}>
+            {num}
+          </button>
+        ))}
+      </div>
+      <div>
+        {Operators.map((calculation) => (
+          <button onClick={() => clickOperatorHandler({ value: calculation })}>
+            {calculation}
+          </button>
+        ))}
+      </div>
     </div>
   );
 
@@ -31,4 +58,4 @@ function render() {
   document.getElementById('app').appendChild(element);
 }
 
-render();
+render(new Calculation(0, 0, ''));
